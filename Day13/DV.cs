1da27232ced64cd214252780809e03b5b356eb64
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-03-06 21:56:09 +0200

    Initial commit

---
+++ Day13/DV.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Day13
+{
+    public partial class DV : Form
+    {
+        public DV()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Day13/GV.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Day13
+{
+    public partial class GV : Form
+    {
+        string CString = System.Configuration.ConfigurationManager.ConnectionStrings["pubsCN"].ConnectionString;
+        public GV()
+        {
+            InitializeComponent();
+        }
+
+        private void GV_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
